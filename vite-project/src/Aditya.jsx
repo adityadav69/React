3a2e399e460cc5f9555027c0f58c995cc622abae
Aditya Yadav <adityayadaw42@gmail.com>
2024-07-23 09:40:29 +0530

    Day 2

---
+++ vite-project/src/Aditya.jsx
@@ -0,0 +1,6 @@
+function Aditya(){
+    return(
+        <h1>Hey i'm Aditya</h1>
+    )
+}
+export default Aditya;
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,12 @@
-
+import Aditya from "./Aditya"
 
 function App() {
 
   return (
+    <>
+    <Aditya/>
   <h1>This is First react app with VITE</h1>
+  </>
   )
 }
 
