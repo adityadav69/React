55f98a7c0d0dd9c54818fdfaf624358ff0e12eb7
Aditya Yadav <adityayadaw42@gmail.com>
2024-07-26 11:53:20 +0530

    Day5

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  let channels=["Chai aur code","Code With Harry","Anuj Bhaiya"]
+
+  return (
+    <>
+     <h1 className='bg-green-500 rounded-lg text-white p-3 mb-10'>Tailwind Test</h1>
+    <Card channels={channels[0]}/>
+    <Card channels={channels[1]} btnText="Visit me"/>
+    <Card channels={channels[2]} btnText="Go"/>
+    </>
+  )
+}
+
+export default App
